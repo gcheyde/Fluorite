35ffa99beaf3d2e57eb8786432e56288776ce1a8
gcheyde <106408303+gcheyde@users.noreply.github.com>
2023-11-08 14:28:35 -0500

    Added the pins and now the LEDs and motors are running concurrently

--- Fluorite.py
+++ Fluorite.py
@@ -2,50 +2,47 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 GPIO.setmode(GPIO.BCM)
-pins = [] #add pin numbers once we figure them out
+pins = [23,24,25] #LED pins
 for i in pins:
     GPIO.setup(i, GPIO.OUT)
-p = GPIO.PWM(,0) #pin numbers to be figured out later
-w = GPIO.PWM(,0)
-m = GPIO.PWM(,0)
-for i in range(100):
-    for i in range(100):
-        p.ChangeDutyCycle(,i)
-        w.ChangeDutyCycle(,(i+33)%100)
-        m.ChangeDutyCycle(,(i+33)%100)
-        sleep(0.1)
+p = GPIO.PWM(23,0) #setting up PWM
+w = GPIO.PWM(24,0)
+m = GPIO.PWM(25,0)
 #motor 1
-GPIO.setup(,GPIO.OUT) #A1
-GPIO.setup(,GPIO.OUT) #A2
-GPIO.setup(,GPIO.OUT) #B1
-GPIO.setup(,GPIO.OUT) #B2
-GPIO.setup(,GPIO.OUT) #sleep
-p1 = GPIO.PWM(,50) #A1
-ppp1 = GPIO.PWM(,50) #A2
-pp1 = GPIO.PWM(,50) #B1
-pppp1 = GPIO.PWM(,50) #B2
-GPIO.output(,GPIO.HIGH)
+GPIO.setup(16,GPIO.OUT) #A1
+GPIO.setup(17,GPIO.OUT) #A2
+GPIO.setup(19,GPIO.OUT) #B1
+GPIO.setup(18,GPIO.OUT) #B2
+GPIO.setup(3,GPIO.OUT) #sleep
+p1 = GPIO.PWM(16,50) #A1
+ppp1 = GPIO.PWM(17,50) #A2
+pp1 = GPIO.PWM(19,50) #B1
+pppp1 = GPIO.PWM(18,50) #B2
+GPIO.output(3,GPIO.HIGH)
 p1.start(0)
 pp1.start(0)
 ppp1.start(0)
 pppp1.start(0)
 #motor 2
-GPIO.setup(,GPIO.OUT) #A1
-GPIO.setup(,GPIO.OUT) #A2
-GPIO.setup(,GPIO.OUT) #B1
-GPIO.setup(,GPIO.OUT) #B2
-GPIO.setup(,GPIO.OUT) #sleep
-p2 = GPIO.PWM(,50) #A1
-ppp2 = GPIO.PWM(,50) #A2
-pp2 = GPIO.PWM(,50) #B1
-pppp2 = GPIO.PWM(,50) #B2
-GPIO.output(,GPIO.HIGH)
+GPIO.setup(13,GPIO.OUT) #A1
+GPIO.setup(6,GPIO.OUT) #A2
+GPIO.setup(4,GPIO.OUT) #B1
+GPIO.setup(5,GPIO.OUT) #B2
+GPIO.setup(2,GPIO.OUT) #sleep
+p2 = GPIO.PWM(13,50) #A1
+ppp2 = GPIO.PWM(6,50) #A2
+pp2 = GPIO.PWM(4,50) #B1
+pppp2 = GPIO.PWM(5,50) #B2
+GPIO.output(2,GPIO.HIGH)
 p2.start(0)
 pp2.start(0)
 ppp2.start(0)
 pppp2.start(0)
-
+#running the motors and changing the LEDs at the same time
 for i in range(150):
+    p.ChangeDutyCycle(i%100)
+    w.ChangeDutyCycle((i+33)%100)
+    m.ChangeDutyCycle((i+33)%100)
     p1.ChangeDutyCycle(100) #1000
     p2.ChangeDutyCycle(100) #1000
     sleep(0.01)
