9a3e97c7e1fe6ba7ac1f8e7046f097441bef1fdd
gcheyde <106408303+gcheyde@users.noreply.github.com>
2023-11-15 14:31:18 -0500

    different configuration for camera

--- cameratest.py
+++ cameratest.py
@@ -1,4 +1,9 @@
-from picamera2 import Picamera2
-#test to capture an image and save it as a test jpg
+from picamera2 import Picamera2, Preview
+import time
 picam2 = Picamera2()
-picam2.start_and_capture_file("test.jpg")
+camera_config = picam2.create_preview_configuration()
+picam2.configure(camera_config)
+picam2.start_preview(Preview.DRM)
+picam2.start()
+time.sleep(2)
+picam2.capture_file("test.jpg")
